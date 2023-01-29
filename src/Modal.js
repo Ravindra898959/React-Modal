0ee6b548d7af9df6eb8b4019e2935366c788c888
Ravindra898959 <ravindra.064464@gmail.com>
2023-01-29 14:48:39 +0530

    btn-changed

--- src/Modal.js
+++ src/Modal.js
@@ -5,10 +5,10 @@ const Modal = ({ closeModal }) => {
     <>
       <div className="modal-div">
         <div className="modal-div2">
-          <button className="btn" onClick={() => closeModal(false)}>
+          <button className="btn-cancel" onClick={() => closeModal(false)}>
             X
           </button>
-          <h1>This is modal</h1>
+          <h1>This is a Modal</h1>
           <h2>Do You want to continue ?</h2>
           <button className="btn">Continue</button>
         </div>
